55fe46a2056e7272c9bce51be0de7c4f1a30912b
ludolacx <lacx.ludovic@gmail.com>
2021-05-18 10:12:26 +0200

    algo fonctionnel avec les 4 variables
    modification de la source des données pour sépales normalisation des données plus des valeurs entrées

--- iris_show_diagram.py
+++ iris_show_diagram.py
@@ -1,48 +1,113 @@
 import pandas
-import matplotlib.pyplot as plt
+# import numpy as np
+import matplotlib.pyplot as plt 
 from sklearn.neighbors import KNeighborsClassifier
+import sklearn
 
 #traitement CSV
-iris=pandas.read_csv("iris.csv")
-x=iris.loc[:,"petal_length"]
-y=iris.loc[:,"petal_width"]
-lab=iris.loc[:,"species"]
+iris=pandas.read_csv("iris2.csv")
+x=(iris.loc[:,"PetalLengthCm"])
+x_scale = sklearn.preprocessing.scale(x)
+# print(x_scale)
+
+y=iris.loc[:,"PetalWidthCm"]
+y_scale = sklearn.preprocessing.scale(y)
+
+SL=iris.loc[:,"SepalLengthCm"]
+SL_scale = sklearn.preprocessing.scale(SL)
+
+SW=iris.loc[:,"SepalWidthCm"]
+SW_scale = sklearn.preprocessing.scale(SW)
+
+I = iris.loc[:"id"] 
+
+lab=iris.loc[:,"Species"]
+
+
 #fin traitement CSV
 
 #valeurs
-longueur=4.9
-largeur=1.6
-k=3
+entree = float(input("longueur pétal ?"))    
+longueur = entree
+l_moy = longueur - x.mean() 
+long_scale = l_moy/x.std()
+
+entree = float(input("largeur pétal ?")) 
+largeur = entree
+la_moy = largeur - y.mean()
+lar_scale = la_moy/y.std()
+
+entree = float(input("longueur sépal ?")) 
+l2=entree
+ls = l2 - SL.mean()
+slong = ls / SL.std()
+
+entree = float(input("largeur sépal ?")) 
+w2=entree
+las= w2 - SW.mean()
+slar = las = SW.std()
+
+k=5
+
+x_var= x_scale+ SL_scale
+y_var =y_scale+SW_scale
 #fin valeurs
 
 #graphique
+plt.subplot(3,1,1)
 plt.axis('equal')
-plt.scatter(x[lab == 0], y[lab == 0], color='g', label='setosa')
-plt.scatter(x[lab == 1], y[lab == 1], color='r', label='versicolor')
-plt.scatter(x[lab == 2], y[lab == 2], color='b', label='virginica')
+plt.scatter(x_var[lab == "Iris-setosa"], y_var[lab == "Iris-setosa"], color='g', label='setosa')
+plt.scatter(x_var[lab == "Iris-versicolor"], y_var[lab == "Iris-versicolor"], color='r', label='versicolor')
+plt.scatter(x_var[lab == "Iris-virginica"], y_var[lab == "Iris-virginica"], color='b', label='virginica')
+plt.scatter(long_scale+slong, lar_scale+slar, color='k')
+plt.xlabel("longueur pétal")
+plt.ylabel("largeur pétal")
+# plt.legend() 
+
+plt.subplot(3,1,2)
+plt.axis('equal')
+plt.scatter(x[lab == "Iris-setosa"], y[lab == "Iris-setosa"], color='g', label='setosa')
+plt.scatter(x[lab == "Iris-versicolor"], y[lab == "Iris-versicolor"], color='r', label='versicolor')
+plt.scatter(x[lab == "Iris-virginica"], y[lab == "Iris-virginica"], color='b', label='virginica')
 plt.scatter(longueur, largeur, color='k')
-plt.legend()
+plt.xlabel("longueur pétal")
+plt.ylabel("largeur pétal")
+# plt.legend() 
+
+plt.subplot(3,1,3)
+plt.axis('equal')
+plt.scatter(SL[lab == "Iris-setosa"], SW[lab == "Iris-setosa"], color='g', label='setosa')
+plt.scatter(SL[lab == "Iris-versicolor"], SW[lab == "Iris-versicolor"], color='r', label='versicolor')
+plt.scatter(SL[lab == "Iris-virginica"], SW[lab == "Iris-virginica"], color='b', label='virginica')
+plt.scatter(l2, w2, color='k')
+plt.xlabel("longueur sépal")
+plt.ylabel("largeur sépal")
+# plt.legend() 
+plt.show()
 #fin graphique
 
 #algo knn
-d=list(zip(x,y))
+d=list(zip(x_scale+SL_scale,y_scale+SW_scale))
 model = KNeighborsClassifier(n_neighbors=k)
 model.fit(d,lab)
-prediction= model.predict([[longueur,largeur]])
-#fin algo knn
+prediction= model.predict([[long_scale+slong,lar_scale+slar]])
+print(long_scale+slong)
+print(lar_scale+slar)
+#fin algo knn 
+
+
 
 #Affichage résultats
-txt="Résultat : "
-if prediction[0]==0:
+txt="Résultat : " 
+if prediction[0]=="Iris-setosa":
   txt=txt+"setosa"
-if prediction[0]==1:
+if prediction[0]=="Iris-versicolor":
   txt=txt+"versicolor"
-if prediction[0]==2:
+if prediction[0]=="Iris-virginica":
   txt=txt+"virginica"
-plt.text(3,0.5, f"largeur : {largeur} cm longueur : {longueur} cm", fontsize=12)
-plt.text(3,0.3, f"k : {k}", fontsize=12)
-plt.text(3,0.1, txt, fontsize=12)
+
 #fin affichage résultats
 
-plt.show()
+
 print(txt)	
+
